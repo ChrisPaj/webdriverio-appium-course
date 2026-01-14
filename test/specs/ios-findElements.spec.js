8a93ae5e1f3a153f72e892835dd3b00c1fb81e98
Christian Pajung <christian.pajung@telekom.de>
2026-01-14 09:26:36 +0100

    find elements by tag name

--- test/specs/ios-findElements.spec.js
+++ test/specs/ios-findElements.spec.js
@@ -4,4 +4,16 @@ describe('iOS find Element', () => {
     await $('~Simple').click()
     await expect(await driver.getAlertText()).toContain("A Short Title Is Best")
   })
+
+  it('find by tag name', async () => {
+    // single element text (because first element)
+    console.log(await $('XCUIElementTypeStaticText').getText());
+
+    // multiple elements
+    const textElements = await $$('XCUIElementTypeStaticText');
+
+    for (const element of textElements) {
+      console.log(await element.getText());
+    }
+  })
 })
