f0dd4e123b890e06553c67dde4eca09a4a8188c2
Christian Pajung <christian.pajung@telekom.de>
2026-01-15 13:44:35 +0100

    scrollable elements

--- test/specs/ios-native.spec.js
+++ test/specs/ios-native.spec.js
@@ -14,7 +14,18 @@ describe('iOS Native Features', () => {
     await expect($('~OK')).not.toExist()
   })
 
-  it('Working with Scrollable elements', async () => {
-    
+  it.only('Working with Scrollable elements', async () => {
+    // easy way - scroll whole screen
+    // await driver.execute('mobile: scroll', { direction: 'down' })
+    // await driver.execute('mobile: scroll', { direction: 'up' })
+
+    // little more komplex: scroll inside element
+    await $('~Picker View').click()
+
+    const redPicker = await $('~Red color component value')
+    const bluePicker = await $('~Blue color component value')
+
+    await driver.execute('mobile: scroll', { element: redPicker.elementId, direction: 'down' })
+    await driver.execute('mobile: scroll', { element: bluePicker.elementId, direction: 'down' })
   })
 })
