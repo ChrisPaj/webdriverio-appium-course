cc50ff4a5bde9226ee5f67783da446e345c9779d
Christian Pajung <christian.pajung@telekom.de>
2026-01-16 13:40:16 +0100

    Page Object Models - ToDo List Items

---
+++ test/screenobjects/ios/item.screen.js
@@ -0,0 +1,45 @@
+class ItemScreen {
+
+  get startItem() {
+    return $('//*[@name="My Second Todo List"]')
+  }
+
+  get createItemButton() {
+    return $('//*[@name="Create item"]')
+  }
+
+  get title() {
+    return $('//*[@value="Title"]')
+  }
+
+  get due() {
+    return $('//*[@value="Due"]')
+  }
+  
+  get datePicker() {
+    return $('~DatePicker.NextMonth')
+  }
+
+  get dateDayOfMonth() {
+    return $('~15')
+  }
+  
+  get donePicker() {
+    return $('~Done')
+  }
+
+get createItem() {
+    return $('~Create')
+  }
+
+itemTextField(text) {
+    return $(`~${text}`)
+  }
+
+itemDueDateField(dueText) {
+    return $(`~${dueText}`)
+  }
+
+}
+
+module.exports = new ItemScreen();
--- test/specs/ios/ios.todo-listPOM.spec.js
+++ test/specs/ios/ios.todo-listPOM.spec.js
@@ -1,4 +1,5 @@
 const ListScreen = require('../../screenobjects/ios/list.screen')
+const ItemScreen = require('../../screenobjects/ios/item.screen')
 
 describe('Todo List', () => {
   it('Create a Todo List', async () => {
@@ -6,5 +7,33 @@ describe('Todo List', () => {
     await ListScreen.listNameInput.setValue('My First Todo List')
     await ListScreen.createBtn.click()
     await expect(await ListScreen.listNameField('My First Todo List')).toBeDisplayed()
+  })
+
+  it.only('Create Todo List with Item(s)', async () => {
+    await ListScreen.createListBtn.click()
+    await ListScreen.listNameInput.setValue('My Second Todo List')
+    await ListScreen.createBtn.click()
+    await expect(await ListScreen.listNameField('My Second Todo List')).toBeDisplayed()
+
+    await ItemScreen.startItem.click()
+    await ItemScreen.createItemButton.click()
+    await ItemScreen.title.setValue('Walk the dog')
+    await ItemScreen.due.click()
+    await ItemScreen.datePicker.click()
+    await ItemScreen.dateDayOfMonth.click()
+    await ItemScreen.donePicker.click()
+    await ItemScreen.createItem.click()
+
+    // Assertions
+
+    const day = 15
+    const date = new Date()
+    date.setMonth(date.getMonth() + 1)
+    const year = date.getFullYear()
+    const monthName = date.toLocaleString('en-US', { month: 'long' })
+
+    const expectedDueText = `Due ${day}. ${monthName} ${year}`
+    await expect(await ItemScreen.itemTextField('Walk the dog')).toBeDisplayed()
+    await expect(await ItemScreen.itemDueDateField(expectedDueText)).toBeDisplayed()
   })
 })
